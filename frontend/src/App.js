96953da1681ed89a523a31b8fd27595255ed0d0c
Rehman Sheikh <sh.rehman222@gmail.com>
2022-08-06 19:17:24 +0500

    Frontend Setup and backend start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div className="App">
-      Hi Empire
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+          </Routes>
+          <Routes>
+            <Route exact path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
